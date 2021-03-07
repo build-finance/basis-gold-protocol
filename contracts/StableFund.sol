cbb26039398ca158264e0e415bf58c9935d1de34
cryptogambler01 <79486794+cryptogambler01@users.noreply.github.com>
2021-03-07 16:55:24 -0500

    Update StableFund.sol

--- contracts/StableFund.sol
+++ contracts/StableFund.sol
@@ -15,50 +15,36 @@ contract StableFund is Operator {
     using SafeMath for uint256;
     
     IUniswapV2Pair public pair;
-    IERC20 public tokenA;
-    IERC20 public tokenB;
+    IERC20 public dai;
+    IERC20 public bsg;
+    address[] public path;
     IUniswapV2Router02 public router;
-    address public trader;
     bool public migrated = false;
     IOracle public goldOracle;
-    uint256 public goldBuyBackPercent; //90%
+    uint256 public goldBuyPercentage; //90%
+    uint256 public goldSellPercentage; //105%
     mapping(address => mapping (address => uint256)) allowed;
     
     constructor(
-        address _tokenA,  //DAI
-        address _tokenB, //BSG
+        address _dai,  //DAI
+        address _bsg, //BSG
         address _factory,
         address _router,
-        address _trader,
         IOracle _goldOracle,
-        uint256 _goldBuyBackPercent
+        uint256 _goldBuyPercentage,
+        uint256 _goldSellPercentage
         
     ) public {
         pair = IUniswapV2Pair(
-            UniswapV2Library.pairFor(_factory, _tokenA, _tokenB)
+            UniswapV2Library.pairFor(_factory, _dai, _bsg)
         );
-        tokenA = IERC20(_tokenA);
-        tokenB = IERC20(_tokenB);
+        path = [_dai, _bsg];
+        dai = IERC20(_dai);
+        bsg = IERC20(_bsg);
         router = IUniswapV2Router02(_router);
-        trader = _trader;
         goldOracle = _goldOracle;
-        goldBuyBackPercent = _goldBuyBackPercent;
-    }
-
-    modifier onlyAllowedTokens(address[] calldata path) {
-        require(
-            (path[0] == address(tokenA) &&
-                path[path.length - 1] == address(tokenB)) ||
-                (path[0] == address(tokenB) &&
-                    path[path.length - 1] == address(tokenA)),
-            'StableFund: tokens are not allowed'
-        );
-        _;
-    }
-
-    modifier onlyTrader() {
-        require(msg.sender == trader, 'sender is not trader');
-        _;
+        goldBuyPercentage = _goldBuyPercentage;
+        goldSellPercentage = _goldSellPercentage;
     }
 
     modifier checkMigration {
@@ -66,8 +52,9 @@ contract StableFund is Operator {
         _;
     }
 
-    /* ========== TRADER ========== */
-    function getGoldPrice() public view returns (uint256) {
+    //Pricing Functions 
+
+    function getGoldPrice() internal view returns (uint256) {
         try goldOracle.price1Last() returns (uint256 price) {
             return price;
         } catch {
@@ -76,9 +63,15 @@ contract StableFund is Operator {
     }
 
     // set to 90% of goldOracle Price
-    function goldPriceCeiling() public view returns(uint256) {
-        return goldOracle.goldPriceOne().mul(uint256(goldBuyBackPercent)).div(100);
+    function goldPriceCeiling() internal view returns(uint256) {
+        return goldOracle.goldPriceOne().mul(uint256(goldBuyPercentage)).div(100);
     }
+
+    function goldPriceFloor() internal view returns(uint256) {
+        return goldOracle.goldPriceOne().mul(uint256(goldSellPercentage)).div(100);
+    }
+
+    //Selling Functions - Seller must be approved before calling sellBSGtoStableFund
     
     function approveSeller(address delegate, uint256 numTokens) public returns (bool) {
         allowed[msg.sender][delegate] = numTokens;
@@ -86,7 +79,7 @@ contract StableFund is Operator {
         return true;
     }
 
-    function buyBSGUnderPeg(uint256 numTokens) public 
+    function sellBSGtoStableFund(uint256 numTokens) public checkMigration
     {
         require(numTokens > 0, 'Stable FUnd: cannot purchase BSG with zero amount');
 
@@ -98,84 +91,92 @@ contract StableFund is Operator {
         );
    
         require(
-            numTokens.mul(goldPriceCeiling()) < IERC20(tokenA).balanceOf(address(this)),
+            numTokens.mul(goldPriceCeiling()) < IERC20(dai).balanceOf(address(this)),
             'StableFund: Not enough DAI for buy'
         );
    
         //Outside my capabilities here lets make sure we test the math...
-        uint256 allowance = tokenB.allowance(msg.sender, address(this));
-        require(allowance >= numTokens, "Check the token allowance");
-        tokenB.transferFrom(msg.sender, address(this), numTokens);
-        tokenA.transferFrom(address(this), msg.sender, numTokens.mul(goldPriceCeiling()).div(goldOracle.price0Last()));
+        uint256 allowance = bsg.allowance(msg.sender, address(this));
+        require(allowance >= numTokens, "Check the token allowance - Call approveSeller");
+        bsg.transferFrom(msg.sender, address(this), numTokens);
+        dai.transferFrom(address(this), msg.sender, numTokens.mul(goldPriceCeiling()).div(goldOracle.price0Last()));
         
         emit StableFundBoughtBSG(msg.sender, numTokens);
     }
 
+    // Stable Fund selling BSG when above peg. - must call approveUniSwap before calling sellBSGOverPeg
 
-    function swapExactTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        uint256 deadline
-    ) public onlyAllowedTokens(path) onlyTrader checkMigration {
-        router.swapExactTokensForTokens(
-            amountIn,
-            amountOutMin,
-            path,
-            address(this),
-            deadline
-        );
-    }
-
-    function swapTokensForExactTokens(
-        uint256 amountOut,
-        uint256 amountInMax,
-        address[] calldata path,
-        uint256 deadline
-    ) public onlyAllowedTokens(path) onlyTrader checkMigration {
-        router.swapTokensForExactTokens(
-            amountOut,
-            amountInMax,
-            path,
-            address(this),
-            deadline
-        );
-    }
-
-    function approve(address token, uint256 amount)
+    function approveUniSwap(address token, uint256 numTokens)
         public
-        onlyTrader
         checkMigration
         returns (bool)
     {
-        if (token == address(tokenA)) {
-            return tokenA.approve(address(router), amount);
+        if (token == address(dai)) {
+            return dai.approve(address(router), numTokens);
         } else {
             require(
-                token == address(tokenB),
-                'StableFund: token should match either tokenA or tokenB'
+                token == address(bsg),
+                'StableFund: token should match either dai or bsg'
             );
-            return tokenB.approve(address(router), amount);
+            return bsg.approve(address(router), numTokens);
         }
     }
 
-    /* ========== OPERATOR ========== */
+    // Can only sell 5 BSG at a time, must have Gold Over 1.05 Peg
+    function sellBSGOverPeg(uint256 numTokens) public  checkMigration 
+    {
+        require(numTokens > 0, 'Stable Fund: cannot sell BSG with zero amount');
+        require(numTokens <= 5, 'Stable Fund: cannot sell more than 5 BSG at a time');
 
-    function setTrader(address _trader) public onlyOperator checkMigration {
-        trader = _trader;
+        uint256 goldPrice = getGoldPrice();
+    
+        require(
+            goldPrice > goldPriceFloor(),
+            'StableFund: BSG not eligible for sale < 105%'
+        );
+   
+        require(
+            numTokens <= IERC20(bsg).balanceOf(address(this)),
+            'StableFund: Not enough BSG to sell'
+        );
+
+        uint256 allowance = bsg.allowance(address(this), address(router));
+        require(allowance >= numTokens, "Check the token allowance - Call approveUniSwap");
+
+        uint deadline = block.timestamp + 30;
+
+        router.swapExactTokensForTokens(
+            numTokens,
+            0,
+            path,
+            address(this),
+            deadline
+        );
+
+        emit StableFundSoldBSG(msg.sender, numTokens);
     }
+
+
 
     /* ========== OWNER ========== */
 
-    function migrate(address target) public onlyOwner checkMigration {
-        IERC20(tokenA).transfer(
+    function setGoldBuyBackPercentage(uint256 _setBSGPercent) public onlyOperator checkMigration {
+        goldBuyPercentage = _setBSGPercent;
+    }
+
+    function setGoldSellPercentage(uint256 _setBSGPercent) public onlyOperator checkMigration {
+        goldSellPercentage = _setBSGPercent;
+    }
+
+    function migrate(address target) public onlyOperator checkMigration {
+        IERC20(dai).transfer(
             target,
-            IERC20(tokenA).balanceOf(address(this))
+            IERC20(dai).balanceOf(address(this))
         );
 
-        IERC20(tokenB).transfer(
+        IERC20(bsg).transfer(
             target,
-            IERC20(tokenB).balanceOf(address(this))
+            IERC20(bsg).balanceOf(address(this))
         );
 
         migrated = true;
@@ -184,5 +185,6 @@ contract StableFund is Operator {
 
     event Migration(address indexed target);
     event StableFundBoughtBSG(address indexed from, uint256 amount);
+    event StableFundSoldBSG(address indexed from, uint256 amount);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
