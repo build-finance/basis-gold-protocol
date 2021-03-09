4cbca356046c26628921b35c8dd4f93340129118
cryptogambler01 <79486794+cryptogambler01@users.noreply.github.com>
2021-03-09 14:10:59 -0500

    Update 9_transfer_operator.js

--- migrations/9_transfer_operator.js
+++ migrations/9_transfer_operator.js
@@ -3,6 +3,7 @@ const Treasury = artifacts.require('Treasury');
 const Gold = artifacts.require('Gold');
 const Bond = artifacts.require('Bond');
 const Share = artifacts.require('Share');
+const StableFund = artifacts.require('StableFund');
 const Timelock = artifacts.require('Timelock');
 
 const DAY = 86400;
@@ -11,6 +12,7 @@ module.exports = async (deployer, network, accounts) => {
   const gold = await Gold.deployed();
   const share = await Share.deployed();
   const bond = await Bond.deployed();
+  const stablefund = await StableFund.deployed();
   const treasury = await Treasury.deployed();
   const boardroom = await Boardroom.deployed();
   const timelock = await deployer.deploy(Timelock, accounts[0], 2 * DAY);
